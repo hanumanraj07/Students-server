cc1904a729333b2454ce0b37ab106ebce41f11a8
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-02-24 15:40:12 +0530

    Added some more students

--- index.js
+++ index.js
@@ -11,74 +11,145 @@ app.use(cors());
 const students = [
   {
     id: 1,
-    name: "Aarav Sharma",
+    name: "Abhishek Malhar",
     branch: "CSE",
     semester: 8,
     cgpa: 9.3
   },
   {
     id: 2,
-    name: "Ishita Verma",
+    name: "Elvish Yadav",
     branch: "IT",
     semester: 7,
     cgpa: 8.9
   },
   {
     id: 3,
-    name: "Rohan Kulkarni",
+    name: "MC Stan",
     branch: "ECE",
     semester: 6,
     cgpa: 8.4
   },
   {
     id: 4,
-    name: "Meera Iyer",
+    name: "Purav Jha",
     branch: "CSE",
     semester: 8,
     cgpa: 9.1
   },
   {
     id: 5,
-    name: "Kunal Deshmukh",
+    name: "Urfi Javade",
     branch: "IT",
     semester: 5,
     cgpa: 7.8
   },
   {
     id: 6,
-    name: "Ananya Reddy",
+    name: "Ananya Pandey",
     branch: "CSE",
     semester: 6,
     cgpa: 8.7
   },
   {
     id: 7,
-    name: "Vikram Patil",
+    name: "Dhruv Rathi",
     branch: "ECE",
     semester: 7,
     cgpa: 8.2
   },
   {
     id: 8,
-    name: "Priyanka Nair",
+    name: "Deepak Kalal",
     branch: "AI",
     semester: 4,
     cgpa: 8.8
   },
   {
     id: 9,
-    name: "Harsh Mehta",
+    name: "Samay Raina",
     branch: "Data Science",
     semester: 5,
     cgpa: 8.0
   },
   {
     id: 10,
-    name: "Neha Gupta",
+    name: "Payal Gaming",
     branch: "CSE",
     semester: 6,
     cgpa: 7.9
+  },
+  {
+    id: 11,
+    name: "Bhuvan Bam",
+    branch: "CSE",
+    semester: 6,
+    cgpa: 8.2
+  },
+  {
+    id: 12,
+    name: "Prajakta Koli",
+    branch: "IT",
+    semester: 5,
+    cgpa: 7.8
+  },
+  {
+    id: 13,
+    name: "CarryMinati",
+    branch: "ECE",
+    semester: 7,
+    cgpa: 8.5
+  },
+  {
+    id: 14,
+    name: "Komal Pandey",
+    branch: "Fashion Tech",
+    semester: 4,
+    cgpa: 8.0
+  },
+  {
+    id: 15,
+    name: "Ashish Chanchlani",
+    branch: "ME",
+    semester: 8,
+    cgpa: 7.5
+  },
+  {
+    id: 16,
+    name: "Technical Guruji",
+    branch: "CSE",
+    semester: 6,
+    cgpa: 8.7
+  },
+  {
+    id: 17,
+    name: "BeerBiceps",
+    branch: "MBA",
+    semester: 3,
+    cgpa: 8.3
+  },
+  {
+    id: 18,
+    name: "Nisha Madhulika",
+    branch: "Food Tech",
+    semester: 2,
+    cgpa: 7.9
+  },
+  {
+    id: 19,
+    name: "Ankur Warikoo",
+    branch: "BBA",
+    semester: 5,
+    cgpa: 8.1
+  },
+  {
+    id: 20,
+    name: "Kusha Kapila",
+    branch: "Mass Comm",
+    semester: 6,
+    cgpa: 7.7
   }
+
 ];
 
 app.get("/students",(req,res)=>{
